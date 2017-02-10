8909928b93cb9f673d961fb16466c01cbd4e5a53
Daniel Rojas <github@danielrojas.net>
2017-02-10 09:13:46 +0100

    Implement CHPD (ESP-01) / RST (NodeMCU) pin functionality

--- TrainTimes-Arduino/wifi_funcs.ino
+++ TrainTimes-Arduino/wifi_funcs.ino
@@ -2,11 +2,20 @@
 
 #include <SoftwareSerial.h>
 
+#define PIN_ESP_CHPD 3
+
 SoftwareSerial wifi(4,2);
+//#define wifi Serial  // DEBUG ONLY!!!
 
 //String WiFiString;
 
 void WiFiInit() {
+  pinMode     (PIN_ESP_CHPD, OUTPUT);
+  digitalWrite(PIN_ESP_CHPD, LOW);
+  delay(100);
+  digitalWrite(PIN_ESP_CHPD, HIGH);
+  pinMode     (PIN_ESP_CHPD, INPUT);
+  
   wifi.begin(9600);
   wifi.setTimeout(50);
 //  WiFiString = "";
