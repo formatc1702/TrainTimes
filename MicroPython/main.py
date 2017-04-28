453beaf30874cc3a77f231cf55251339338ed96c
Daniel Rojas <github@danielrojas.net>
2017-04-28 10:57:07 +0200

    Clean up data structure

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -11,14 +11,55 @@ now = ntp.get_time()
 print(time.localtime(now))
 
 a = vbbapi.API()
-sid     = a.get_station_id("Strassmannstr")
-dirid_n = a.get_station_id("Eberswalder")
-dirid_s = a.get_station_id("Bersarinplatz")
 
-n_t = a.get_departures(sid, 5, dirid_n, True)
-for t in n_t:
-    print(t, time.localtime(t), "departs in {:+03}:{:02}".format((t - now) // 60, (t - now) % 60))
+def get_station_ids(stations):
+    ids = {}
+    for s in stations:
+        ids[s] = a.get_station_id(s, True)
+    return ids
 
-s_t = a.get_departures(sid, 5, dirid_s, True)
-for t in s_t:
-    print(t, time.localtime(t), "departs in {:+03}:{:02}".format((t - now) // 60, (t - now) % 60))
+def get_departures(origin, direction):
+    # oid = a.get_station_id(origin)
+    # did = a.get_station_id(direction)
+
+    return a.get_departures(origin, 5, direction, True)
+
+stations = ["Strassmannstr",
+            "Frankfurter_Tor",
+            "Eberswalder",
+
+            "S_Landsberger_Allee",
+            "S_Gesundbrunnen",
+            "S_Neukolln",
+            "S_Bornholmer_Strasse",
+            "S_Schoneweide",
+
+            "Bersarinplatz",
+            "Lichtenberg_Gudrunstr",
+            "Wilhelminenhofstr",
+            ]
+
+sids = get_station_ids(stations)
+sids["Bersarinplatz"] = "9120816"
+print(sids)
+gc.collect()
+print(gc.mem_free())
+
+reqs = [
+        ("Strassmannstr",      "Frankfurter_Tor"),
+        ("Strassmannstr",      "Eberswalder"),
+        ("S_Landsberger_Allee","S_Gesundbrunnen"),
+        ("S_Landsberger_Allee","S_Neukolln"),
+        ("S_Landsberger_Allee","S_Bornholmer_Strasse"),
+        ("S_Landsberger_Allee","S_Schoneweide"),
+        ("Bersarinplatz",      "Lichtenberg_Gudrunstr"),
+        ("Bersarinplatz",      "Wilhelminenhofstr")
+       ]
+
+for origin, direction in reqs:
+    print(origin, direction)
+    departures = get_departures(sids[origin], sids[direction])
+    for t in departures:
+        print(t, time.localtime(t), "departs in {:+03}:{:02}".format((t - now) // 60, (t - now) % 60))
+    gc.collect()
+    print(gc.mem_free())
