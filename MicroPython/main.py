69ee027d230b182c23b013c61640ddaa6ef3468d
Daniel Rojas <github@danielrojas.net>
2017-04-28 11:04:54 +0200

    Further clean up

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -11,43 +11,12 @@ now = ntp.get_time()
 print(time.localtime(now))
 
 a = vbbapi.API()
-
-def get_station_ids(stations):
-    ids = {}
-    for s in stations:
-        ids[s] = a.get_station_id(s, True)
-    return ids
-
 def get_departures(origin, direction):
-    # oid = a.get_station_id(origin)
-    # did = a.get_station_id(direction)
-
     return a.get_departures(origin, 5, direction, True)
 
-stations = ["Strassmannstr",
-            "Frankfurter_Tor",
-            "Eberswalder",
-
-            "S_Landsberger_Allee",
-            "S_Gesundbrunnen",
-            "S_Neukolln",
-            "S_Bornholmer_Strasse",
-            "S_Schoneweide",
-
-            "Bersarinplatz",
-            "Lichtenberg_Gudrunstr",
-            "Wilhelminenhofstr",
-            ]
-
-sids = get_station_ids(stations)
-sids["Bersarinplatz"] = "9120816"
-print(sids)
-gc.collect()
-print(gc.mem_free())
-
 reqs = [
-        ("Strassmannstr",      "Frankfurter_Tor"),
         ("Strassmannstr",      "Eberswalder"),
+        ("Strassmannstr",      "Frankfurter_Tor"),
         ("S_Landsberger_Allee","S_Gesundbrunnen"),
         ("S_Landsberger_Allee","S_Neukolln"),
         ("S_Landsberger_Allee","S_Bornholmer_Strasse"),
@@ -55,6 +24,21 @@ reqs = [
         ("Bersarinplatz",      "Lichtenberg_Gudrunstr"),
         ("Bersarinplatz",      "Wilhelminenhofstr")
        ]
+
+sids = {}
+for origin, direction in reqs:
+    if not origin in sids:
+        sids[origin] = a.get_station_id(origin)
+        print("Got ID for {}: {}".format(origin, sids[origin]))
+    if not direction in sids:
+        sids[direction] = a.get_station_id(direction)
+        print("Got ID for {}: {}".format(direction, sids[direction]))
+
+sids["Bersarinplatz"] = "9120816" # Bersarin (Weidenweg) can't be found by location.name?
+print("Got all IDs!")
+
+gc.collect()
+print(gc.mem_free())
 
 for origin, direction in reqs:
     print(origin, direction)
