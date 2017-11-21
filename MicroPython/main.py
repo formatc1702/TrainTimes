bdcddf89c6be4442ccc34de6be6c588afd3cc064
Daniel Rojas <github@danielrojas.net>
2017-11-21 16:52:26 +0100

    Create some fake data for the FabContest

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -2,103 +2,42 @@
 import utime as time
 import gc
 # custom modules
-import wifi
-import ntp
-import vbbapi
 from machine import UART
 import esp
 
-debugging = False
+debugging = True
 
-# connect to WiFi
-w = wifi.WiFi()
 
-# get current time from NTP server
-now = ntp.get_time()
-if debugging:
-    print(time.localtime(now))
+# generate random demo data
 
-# connect to VBB API server
-a = vbbapi.API()
-def get_departures(origin, direction): # shorthand function
-    return a.get_departures(origin, 5, direction, False)
+time.sleep(3)
 
-# configure the request
-reqs = [
-        ("Strassmannstr",      2, "Eberswalder"),
-        ("Strassmannstr",      2, "Frankfurter_Tor"),
-        ("S_Landsberger_Allee",5,"S_Gesundbrunnen"),
-        ("S_Landsberger_Allee",5,"S_Neukolln"),
-        ("S_Landsberger_Allee",5,"S_Bornholmer_Strasse"),
-        ("S_Landsberger_Allee",5,"S_Schoneweide"),
-        ("Bersarinplatz",      5,"Lichtenberg_Gudrunstr"),
-        ("Bersarinplatz",      5,"Wilhelminenhofstr")
-       ]
-num_departures = 5
+deps = []
 
-# read station IDs
-station_ids = {}
-# check file for cached IDs
-with open("stationids.txt","r") as f:
-    while True:
-        line = f.readline()
-        if line:
-            sid, sname = line.split(' ')
-            station_ids[sname.strip()] = sid.strip()
-            if debugging:
-                print("ID {} is {}".format(sid.strip(), sname.strip()))
+def randint(_range):
+    return int.from_bytes(uos.urandom(1), 'little')/255*_range
+
+def genrand(num, interval, variance):
+    _deps = []
+    for n in range(num):
+        x = round(60*(n*interval+randint(variance)+randint(interval)+1))
+        if x > 0 and x/60 < 100:
+            _deps.append(x)
         else:
-            break
-# get missing IDs from API
-for origin, walktime, direction in reqs:
-    if not origin in station_ids:
-        station_ids[origin]    = a.get_station_id(origin)
-        if debugging:
-            print("New ID {} is {}".format(station_ids[origin],    origin))
-    if not direction in station_ids:
-        station_ids[direction] = a.get_station_id(direction)
-        if debugging:
-            print("New ID {} is {}".format(station_ids[direction], direction))
-# finished
-if debugging:
-    print("Got all IDs!")
+            print("whaaat {}".format(x))
+            _deps.append(-999)
+    return _deps
 
-# free up some memory?
-gc.collect()
-if debugging:
-    print(gc.mem_free())
-
-# get departure times
-out = []
-for origin, walktime, direction in reqs: # iterate over each origin/direction tuple
-    if debugging:
-        print("{} -> {}".format(origin, direction))
-    # request departure times from API
-    departures = get_departures(station_ids[origin], station_ids[direction])
-    _out = []
-    for t in departures: # iterate over each received departure time
-        # how much until departure?
-        timediff = t - now
-        if debugging:
-            print(t, time.localtime(t), timediff,
-                  "departs in {: 03}:{:02}".format(timediff // 60, timediff % 60))
-        # should we keep this departure?
-        if timediff > 0: # is it in the future?
-            if timediff < 99 * 60: # is it in the next 99 mins but at least 2 min in future?
-                if timediff > walktime * 60:
-                    _out.append(timediff)
-            else: # add placeholder if not
-                _out.append("-999")
-    # add placeholders for padding
-    if len(_out) < num_departures:
-        for i in range(len(_out), num_departures):
-            _out.extend(["-999"])
-    # add to list
-    out.append(_out)
-
-    gc.collect()
-    if debugging:
-        print(gc.mem_free())
+deps.extend(genrand(5, 5,4))
+deps.extend(genrand(5, 5,4))
+deps.extend(genrand(5, 7,5))
+deps.extend(genrand(5, 7,5))
+deps.extend(genrand(5, 7,5))
+deps.extend(genrand(5, 7,5))
+deps.extend(genrand(3, 20,2))
+deps.extend([-999, -999])
+deps.extend(genrand(3, 20,2))
+deps.extend([-999, -999])
 
 # Output data to Arduino
 print("BEGIN SEND")
@@ -106,23 +45,18 @@ uart = UART(1,9600) # TX: GPIO2=D4, RX: none? (GPIO is also LED!)
 time.sleep(0.1)
 uart.write('{')
 uart.write('\n')
-for dirs in out:
-    for deps in dirs:
-        uart.write("{} \n".format(deps))
-    time.sleep(0.05) # give Arduino time to read the buffer
+for dep in deps:
+    uart.write("{} \n".format(dep))
+    time.sleep(0.01) # give Arduino time to read the buffer
 uart.write("}")
 uart.write('\n')
 uart.write('\n')
 print("END SEND")
 print("")
-for dirs in out:
-    for deps in dirs:
-        print     ("{}\t".format(deps), end="")
-        # print     ("{:>4} \t".format(deps), end="")
+for dep in deps:
+    print     ("{}\t".format(dep), end="")
+    # print     ("{:>4} \t".format(deps), end="")
     print("")
 print("")
 
-if debugging == False:
-    w.wlan.active(False)
-    print("Good night!")
-    esp.deepsleep()
+# no deep sleep for demo
